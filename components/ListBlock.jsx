5696fdd2810d35115a7c133e463ee2b17fef2d95
Daniel Chicea <danielc@directferries.com>
2025-03-08 22:29:48 +0200

    [chaos] Layout fixes

--- components/ListBlock.jsx
+++ components/ListBlock.jsx
@@ -1,14 +1,29 @@
 "use client";
 import styles from "/styles/ListBlock.module.css";
 
-export default function ListBlock({ title, items = [] }) {
+export default function ListBlock({title, items = []}) {
     return (
         <div className={styles.listContainer}>
             <h2 className={styles.listTitle}>{title}</h2>
             <ul className={styles.list}>
-                {items.map((item, idx) => (
-                    <li key={idx}>{item}</li>
-                ))}
+                {items.map((item, idx) => {
+                    if (item.startsWith("Telefon")) {
+                        const phoneNumber = item.replace("Telefon:", "").trim();
+                                                   
+                        return <li key={idx}>
+                             <a href={`tel:+4${phoneNumber.replace(/\s/g, "")}`}>
+                                 📞 Telefon: {phoneNumber}
+                            </a>
+                        </li>
+                    }
+                    else {
+                        return (
+                            <li key={idx}>{item}</li>
+                        );
+                    }
+
+                    
+                })}
             </ul>
         </div>
     );
